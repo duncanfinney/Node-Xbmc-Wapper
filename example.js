212fcb26a6844a32d86d305919c3ee6c0bc91675
Duncan Finney <duncan.finney@gmail.com>
2014-06-17 15:13:01 -0400

    Initial commit

---
+++ example.js
@@ -0,0 +1,11 @@
+﻿var serviceDefinition = require('xbmcrpc.json');
+var config = {
+	"XbmcHost": "127.0.0.1",
+	"XbmcPort": 8081,
+	"DebugLogging": true,
+    "XbmcServiceDefinition": serviceDefinition
+}
+var xbmc = require('lib/xbmc.js')(config);
+xbmc.JSONRPC.Introspect({}, function(err, results) {
+    console.log(results);
+});
